061772f74146dd68ffd5cf5b39a04bf338097ac1
Vladimir Toropov <toropov29@rambler.ru>
2019-12-11 13:33:50 +0300

    start to do 5 lab of os

---
+++ 5lab/cmake-build-debug/CMakeFiles/5lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5lab.exe"
+  "5lab.exe.manifest"
+  "5lab.pdb"
+  "CMakeFiles/5lab.dir/main.cpp.obj"
+  "lib5lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
